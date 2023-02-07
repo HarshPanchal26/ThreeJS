63788312713d850d37e70c73b7b5b0b387fd367d
unknown <hrppanchal27@gmail.com>
2023-02-07 23:26:16 +0530

    bascis of three js

--- README.md
+++ README.md
@@ -68,3 +68,14 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+
+
+three js 
+
+camera:
+
+1 . Pespective camera 
+2 . Orthogonal camera 
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+import * as THREE from 'three';
 import './App.css';
 
 function App() {
+  
+  const renderer = new THREE.WebGLRenderer();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  const scene = new THREE.Scene();
+
+  const camera = new THREE.PerspectiveCamera(75 , window.innerWidth / window.innerHeight , 0 , 1000);
+  
+  // add helper 
+  const axesHelper = new THREE.AxesHelper(5);
+  scene.add(axesHelper);
+
+  camera.position.z = 15;
+  
+  renderer.render(scene,camera);
+
+  
+  
+  document.getElementById('app') && document.getElementById('app').appendChild(renderer.domElement);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id='app'>
+      {/* <header className="App-header">
+        <h1>dcdnck</h1>
+      </header> */}
     </div>
   );
 }
